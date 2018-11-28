48788a2ed12589d059dcdecd9198b804ecd8d811
KV <kirk.voss@gmail.com>
2018-11-28 10:46:21 -0600

    Added seasons_number weighted mean function

--- NBAwinshare/source/helper_functions.py
+++ NBAwinshare/source/helper_functions.py
@@ -1,7 +1,13 @@
+import pandas as pd
+import numpy as np
 
-def weighted_mean(group, avg_name, weight_name):
+def weighted_mean_one_col_weight(df, cols, weights='Seasons_number'):
     '''
     Computes a weighted mean -- for calling with pandas groupby.agg functionality
+    With the weights being in one column
+    '''
+    return pd.Series(np.average(df[cols], weights=df[weights], axis=0), cols)
+
     '''
     d = group[avg_name]
     w = group[weight_name]
@@ -10,3 +16,4 @@ def weighted_mean(group, avg_name, weight_name):
         return (d*w).sum() / w.sum()
     except ZeroDivisionError:
         return d.mean()
+    '''
